584f82d6e0267067ceae9963bd0e4354c9aad9f6
bhaveshgupta01 <bhaveshgupta01@gmail.com>
2022-08-25 00:58:38 +0530

    Vulneribility Data 2.0

--- lib/Screens/VulnerabiltyData.dart
+++ lib/Screens/VulnerabiltyData.dart
@@ -1,157 +1,238 @@
 import 'dart:async';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:nxsam_bg/Models/VulData.dart';
 import 'package:nxsam_bg/Screens/Vulnerability.dart';
 
 class VulnerData extends StatefulWidget {
-  final VulData DataHere;
+  final VulData dataHere;
 
   const VulnerData({
     Key? key,
-    required this.DataHere,
+    required this.dataHere,
   }) : super(key: key);
 
   @override
   State<StatefulWidget> createState() {
 
-    return DataShow(DataHere: DataHere);
+    return DataShow(dataHere: dataHere);
   }
 }
 
 class DataShow extends State<VulnerData> {
-  final VulData DataHere;
+  final VulData dataHere;
 
   DataShow({
-    required this.DataHere,
+    required this.dataHere,
   });
 
+  static var _approval = ['Approved','Approved: Yes','Approved: No' , ];
+  var dropDownApprovalItem= _approval[0];
+  static var _validate = ['Validated','Validated: Yes','Validated: No' , ];
+  var dropDownValidateItem= _validate[0];
+  
   @override
   Widget build(BuildContext context){
-  return Scaffold(
+  return
+    // WillPopScope(
+    // onWillPop: () {Navigator.pop(context);},
+    //
+    // child:
+    Scaffold(
 
-    appBar: AppBar(
-      leading: IconButton(onPressed:(){Navigator.pop(context);} , icon: const Icon(Icons.arrow_back_ios_outlined), color: Colors.black,),
-      title: const Text("Vulnerabilities", style: TextStyle(color: Colors.indigo, fontSize: 20)),
-      backgroundColor: Colors.white,
-      ),
-    body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child:
-          ListView(
-            scrollDirection: Axis.vertical,
-            shrinkWrap: true,
-            children: [
-              Row(
-                children: [
-
-                ],
-              ),
-              Card(
-                child: ListView(
-                  shrinkWrap: true,
-                  children: <Widget>[
-                    Expanded(child: ListTile(
-                      title: Text(DataHere.name),
-                      subtitle: Text(DataHere.link),
-                    )),
-
-                    Expanded(child: Padding(
-                      padding: const EdgeInsets.fromLTRB(8,12,8,2),
-                      child: ListTile(
-                        title: Row(
-                          children: [
-                            const Text("Artifact"),
-                            IconButton(icon: Icon(Icons.add_circle), color: Colors.indigo, onPressed: () {  },),
-                          ],
-                        )
-                      ),
-                    )),
-                    //LIST OF LIST OF ARTIFACTS
-                    Expanded(child: ListView.builder(
-                      shrinkWrap: true,
-                      itemCount: DataHere.Artifacts.length,
-                      itemBuilder: (BuildContext context, index)
-                      {
-                        return ListView(
-                          shrinkWrap: true,
-                          children: <Widget>[
-                            Card(
-                              child: ListView(
-                                shrinkWrap: true,
-                                children: <Widget>[
-                                  ListTile(
-                                    title: Text("CASE ${index+1})"),
-                                  ),
-                                  ListView.builder(
-                                      shrinkWrap: true,
-                                      itemCount: DataHere.Artifacts[index].length,
-                                      itemBuilder: (BuildContext context, indexa)
-                                      {
-                                        return ListTile(
-                                          title: Text("step ${indexa+1})"),
-                                          subtitle: Text(
-                                            DataHere.Artifacts[index][indexa],
-                                            style: TextStyle(
-                                                fontSize: 20, color: Colors.indigo),
-                                          ),
-                                          trailing: IconButton(
-                                            onPressed: () {  },
-                                            icon: Icon(Icons.more_vert),
-                                            color: Colors.indigo,),
-                                        );
-                                      }
-                                  )
-                                ],
-                              ),
-
-                            )
-                          ],
-                        );
-                      }
-                    )),
-
-                    Expanded(child: Padding(
-                      padding: const EdgeInsets.fromLTRB(8,12,8,2),
-                      child: ListTile(
-                          title: Row(
-                            children: [
-                              const Text("Comments"),
-                              IconButton(
-                                icon: Icon(Icons.add_circle),
-                                color: Colors.indigo,
-                                onPressed: () {  },),
-                            ],
-                          )
-                      ),
-                    )),
-                    //LIST OF comments
-                    Expanded(
-                      child: Card(
-                        margin: EdgeInsets.all(10),
-                        child: ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: DataHere.Comment.length,
-                          itemBuilder: (BuildContext context, index){
-                            return ListView(
-                              shrinkWrap: true,
-                              children: <Widget>[
-                                ListTile(
-                                  title: Text(DataHere.Comment[index]),
-                                  subtitle: Text('by ${DataHere.CommentAuthor[index]}'),
-                                  trailing: Text('3 hours ago'),
-                                )
-                              ],
+      appBar: AppBar(
+        leading: IconButton(onPressed:(){Navigator.pop(context);} , icon: const Icon(Icons.arrow_back_ios_outlined), color: Colors.black,),
+        title: const Text("Vulnerabilities", style: TextStyle(color: Colors.indigo, fontSize: 20)),
+        backgroundColor: Colors.white,
+        ),
+      body: ListView(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15.0),
+                      child: DropdownButton(
+                          items: _approval.map((dropDownApprovalItem) {
+                            return DropdownMenuItem<String> (
+                              value: dropDownApprovalItem,
+                              child: Text(dropDownApprovalItem),
                             );
+                          }).toList(),
+
+                          value: dropDownApprovalItem,
+
+                          onChanged: (valueSelectedByUser)
+                          {
+                            setState(() {
+                           // text here
+                            });
                           }
-                        ),
-                      )
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12.0),
+                      child: DropdownButton(
+                          items: _validate.map((dropDownValidateItem) {
+                            return DropdownMenuItem<String> (
+                              value: dropDownValidateItem,
+                              child: Text(dropDownValidateItem),
+                            );
+                          }).toList(),
+
+                          value: dropDownValidateItem,
+
+                          onChanged: (valueSelectedByUser)
+                          {
+                            setState(() {
+                              // text here
+                            });
+                          }
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(CheckPriority(dataHere.priority),
+                        style: TextStyle(fontSize: 15),),
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
-    )
+                Card(
+                  child: ListView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    children: <Widget>[
+                      Expanded(child: ListTile(
+                        title: Text(dataHere.name, style: const TextStyle(color: Colors.indigo),),
+                        subtitle: Text(dataHere.link),
+                      )),
+
+                      Expanded(child: Padding(
+                        padding: const EdgeInsets.fromLTRB(8,12,8,0),
+                        child: ListTile(
+                          title: const Text("Artifact"),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.add_circle),
+                            color: Colors.indigo,
+                            onPressed: () {  },
+                            padding: const EdgeInsetsDirectional.only(end: 5.0),
+                          iconSize: 30,),
+                        ),
+                      )),
+                      //LIST OF LIST OF ARTIFACTS
+                      Expanded(child: ListView.builder(
+                        shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: dataHere.Artifacts.length,
+                        itemBuilder: (BuildContext context, index)
+                        {
+                          return ListView(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(10,5,10,5),
+                                child: Card(
+                                  margin: const EdgeInsets.fromLTRB(10,0,10,0),
+                                  elevation: 5,
+                                  child: ListView(
+                                    physics: const NeverScrollableScrollPhysics(),
+                                    shrinkWrap: true,
+                                    children: <Widget>[
+                                      ListTile(
+                                        title: Text("CASE ${index+1})"),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(10,0,10,0),
+                                        child: ListView.builder(
+                                            physics: const NeverScrollableScrollPhysics(),
+                                            shrinkWrap: true,
+                                            itemCount: dataHere.Artifacts[index].length,
+                                            itemBuilder: (BuildContext context, indexa)
+                                            {
+                                              return ListTile(
+                                                title: Text("step ${indexa+1})"),
+                                                subtitle: Text(
+                                                  dataHere.Artifacts[index][indexa],
+                                                  style: const TextStyle(
+                                                      fontSize: 20, color: Colors.indigo),
+                                                ),
+                                                trailing: IconButton(
+                                                  onPressed: () {  },
+                                                  icon: const Icon(Icons.more_vert),
+                                                  color: Colors.indigo,),
+                                              );
+                                            }
+                                        ),
+                                      )
+                                    ],
+                                  ),
+
+                                ),
+                              )
+                            ],
+                          );
+                        }
+                      )),
+
+                      Expanded(child: Padding(
+                        padding: const EdgeInsets.fromLTRB(8,12,8,2),
+                        child: ListTile(
+                            title:
+                                const Text("Comments"),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.add_circle),
+                            color: Colors.indigo,
+                            onPressed: () {  },
+                            padding: const EdgeInsetsDirectional.only(end: 5.0),
+                            iconSize: 30,),
+                        ),
+                      )),
+                      //LIST OF comments
+                      Expanded(
+                        child: Card(
+                          margin: const EdgeInsets.fromLTRB(10,0,10,0),
+                          child: ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                            itemCount: dataHere.Comment.length,
+                            itemBuilder: (BuildContext context, index){
+                              return ListView(
+                                physics: const NeverScrollableScrollPhysics(),
+                                shrinkWrap: true,
+                                children: <Widget>[
+                                  ListTile(
+                                    title: Text(dataHere.Comment[index]),
+                                    subtitle: Text('by ${dataHere.CommentAuthor[index]}'),
+                                    trailing: const Text('3 hours ago'),
+                                  )
+                                ],
+                              );
+                            }
+                          ),
+                        )
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+    // ),
   );
+  }
+  String CheckPriority(int value){
+    String priority;
+    switch(value)
+    {
+      case 1: priority="High";
+      break;
+      case 2: priority="Medium";
+      break;
+      case 3: priority="Low";
+      break;
+      default: priority="Low";
+    }
+    return priority;
   }
 }
